dcc6ac81b65fc4d558588e370f1de4b46337eff6
Alexander <123tachka123@gmail.com>
2019-11-16 16:07:32 +0200

    some fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import dataPicker from './js/dataPicker'
 import './styles.css';
+import validator from './js/validator'
 
 
-
+validator()
 dataPicker();
 
---
+++ src/js/validator.js
@@ -0,0 +1,38 @@
+const form = document.querySelector('form');
+const inputs = form.querySelectorAll('input');
+
+export default function() {
+  form.addEventListener('submit', submitForm);
+  inputs.forEach(input => {
+    input.addEventListener('blur', validation);
+    input.addEventListener('input', validation);
+  });
+}
+
+function validation(e) {
+  const span = e.target.parentNode.querySelector('span');
+  setTimeout(() => {
+    if (!e.target.value) {
+      span.style.display = 'block';
+    } else {
+      span.style.display = 'none';
+    }
+  }, 200);
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  const processValidation = submitValidation();
+  if (processValidation.length) {
+    processValidation.forEach(elem => {
+      elem.parentNode.querySelector('span').style.display = 'block';
+    });
+    return;
+  }
+  form.innerHTML = 'Success';
+}
+
+function submitValidation() {
+  const listOfInputs = Array.from(inputs);
+  return listOfInputs.filter(elem => !elem.value);
+}
